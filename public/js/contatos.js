844dc08cf40166169ac8ad0352922f135c85f682
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-02-22 15:03:38 -0300

    studying the way that angularjs use

--- public/js/contatos.js
+++ public/js/contatos.js
@@ -3,6 +3,29 @@
 var app = angular.module("myApp", []);
 
 app.controller("myController", function($scope){
+  debugger;
   $scope.firstName = "Geronimo";
   $scope.secondName = "Olanda";
+
+  //functions
+  $scope.changeName = function(boolChange = false, boolconcate = false){
+    if(boolChange){
+      $scope.firstName = "Thalia";
+      $scope.secondName = "Gostosa";
+    
+    }
+    
+    if(boolconcate){
+      $scope.message = $scope.firstName + " " + $scope.secondName;
+    
+    }
+    
+    else{
+      $scope.firstName = "";
+      $scope.secondName = "";
+      $scope.message = "O zé gotinha, ainda não foi passado como true, kita daqui"
+    
+    
+    }
+  }
 })
