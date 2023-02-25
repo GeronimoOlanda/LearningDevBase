782e2f5035a0d04e48607ca9b8ff17eb790f3acd
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-02-24 22:15:39 -0300

    adicionando o js do nosso todo list

---
+++ public/js/Todo.js
@@ -0,0 +1,25 @@
+const app = angular.module("MyApp", []);
+
+app.controller("todoController", ($scope) => {
+  $scope.todoList = [{
+    todoText: 'Exemplo: Tirar Areia dos Pets',
+    done: false
+  }];
+
+    $scope.todoAdd = () => {
+        $scope.todoList.push({
+        todoText: $scope.todoInput,
+            done: false
+    });
+    
+    $scope.todoInput = "";
+
+    };
+    $scope.remove = function() {
+      var oldList = $scope.todoList;
+      $scope.todoList = [];
+      angular.forEach(oldList, function(x) {
+          if (!x.done) $scope.todoList.push(x);
+      });
+  };
+});
