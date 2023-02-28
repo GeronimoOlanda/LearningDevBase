9b97ec637d609fe3cb9fb968929701ab18528d9e
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-02-27 22:45:07 -0300

    estudando js

---
+++ public/js/fundamentos.js
@@ -0,0 +1,11 @@
+let nome = "Geronimo";
+const sobrenome = "Olanda";
+
+const ListaAlimentos = ["Arroz", "Feijão", "Carne", "Frango", "Salsicha", "Linguiça"];
+
+const DadosCompletos = {
+  NomeCompleto: 'Geronimo Olanda',
+  Religiao: "Cristão",
+  TemploFrequentado: "Congregação Cristã no Brasil",
+  AnosImportantes: [2023, 2019, 2020, "08/04/2022"] 
+}
